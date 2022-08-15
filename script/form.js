87a6f4b0fef2fee8394cf7b57deb4b9ba8b77319
Sadiq <seeker99157@gmial.com>
2022-08-15 10:33:21 +0100

    reversing backend stuff

--- script/form.js
+++ script/form.js
@@ -1,11 +1,11 @@
 const nameEl = document.getElementById('name');
-const address = document.getElementById('address');
-const state = document.getElementById('state');
-const number = document.getElementById('number');
-const emailInput = document.getElementById('email-input');
+const addressEl = document.getElementById('address');
+const stateEl = document.getElementById('origin');
+const numberEl = document.getElementById('number');
+const emailEl = document.getElementById('email-input');
+const profEl = document.getElementById('prof');
+const nationEl = document.getElementById('nation');
 const form = document.getElementById('form');
-
-// check name
 
 const checkName = () => {
 
@@ -21,11 +21,117 @@ const checkName = () => {
     return valid;
 }
 
-form.addEventListener('submit', (e)=> {
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkAddress = () => {
+
+    let valid = false;
+    const address = addressEl.value.trim();
+
+    if (!isRequired(address)) {
+        showError(addressEl, 'Address cannot be blank.');
+    } else {
+        showSuccess(addressEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkState = () => {
+
+    let valid = false;
+    const state = stateEl.value.trim();
+
+    if (!isRequired(state)) {
+        showError(stateEl, 'State cannot be blank.');
+    } else {
+        showSuccess(stateEl);
+        valid = true;
+        console.log(stateEl.value);
+    }
+    return valid;
+}
+
+const checkProf = () => {
+
+    let valid = false;
+    const prof = profEl.value.trim();
+
+    if (!isRequired(prof)) {
+        showError(profEl, 'Profession cannot be blank.');
+    } else {
+        showSuccess(profEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkNationality = () => {
+
+    let valid = false;
+    const nation = nationEl.value.trim();
+
+    if (!isRequired(nation)) {
+        showError(nationEl, 'Nationality cannot be blank.');
+    } else {
+        showSuccess(nationEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkNumber = () => {
+
+    let valid = false;
+    const number = numberEl.value.trim();
+
+    if (!isRequired(number)) {
+        showError(numberEl, 'Number cannot be blank.');
+    } else {
+        showSuccess(numberEl);
+        valid = true;
+    }
+    return valid;
+}
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
     e.preventDefault();
-    console.log('foo');
-    checkName()
-})
+
+    // validate fields
+    let isNameValid = checkName(),
+        isEmailValid = checkEmail(),
+        isAddressValid = checkAddress(),
+        isStateValid = checkState(),
+        isProfValid = checkProf(),
+        isNationValid = checkNationality(),
+        isNumberValid = checkNumber();
+
+    let isFormValid = isNameValid &&
+        isEmailValid &&
+        isAddressValid &&
+        isStateValid &&
+        isProfValid &&
+        isNationValid &&
+        isNumberValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        console.log(nameEl.value);
+    }
+});
 
 // utility function
 
@@ -64,3 +170,50 @@ const showSuccess = (input) => {
     error.textContent = '';
 }
 
+const debounce = (fn, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'name':
+            checkName();
+            break;
+        case 'email-input':
+            checkEmail();
+            break;
+        case 'address':
+            checkAddress();
+            break;
+        case 'origin':
+            checkState();
+            break;
+        case 'number':
+            checkNumber();
+            break;
+        case 'nation':
+            checkNationality();
+            break;
+        case 'prof':
+            checkProf();
+            break;
+    }
+}));
+
+
+
+// pushing the data to atlas
+
+app.listen(3000, ()=> {
+    console.log('server running');
+})
