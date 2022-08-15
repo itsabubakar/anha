80cac841cb990721a8739a2f9b351491c3f748bd
AbdulkarimOgaji <abdulkarimogaji002@gmail.com>
2022-08-15 12:37:03 +0100

    fix: backend

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static('./public_html'))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const uri = 'mongodb+srv://seeker:blackwolf99157@cluster0.5ncyljk.mongodb.net/Collection0';
+const uri = process.env.DB_URI;
 
-async function connect () {
-    try {
-        await mongoose.connect(uri);
-        console.log('connected');
-    } catch (error) {
-        console.log(error);
-    }
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("connected");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 connect();
 
-const notesSchema = {
-    title: String,
-    content: String
-}
+const anhaSchema = {
+  fullName: String,
+  address: String,
+  state: String,
+  phone: String,
+  email: String,
+  maritalStatus: String,
+  profession: String,
+  nationality: String,
+  referredBy: String,
+  identification: String,
+  membershipType: String,
+  reasonForJoin: String,
+  farm: String,
+};
 
-const Note = mongoose.model('Note', notesSchema)
+const Anha = mongoose.model("Note", anhaSchema);
 
-app.post(('/join-us'), (req, res)=> {
-    let newNote = new Note({
-        title: req.body.name,
-        content: req.body.address,
-    });
-    newNote.save()
-    console.log(newNote);
-    res.redirect('/pages/join-us.html');
-})
+app.get("/healthCheck", (req, res) => {
+  res.json({ status: "ok" });
+});
 
-// app.listen(8080, ()=> {
-//     console.log('server started on 8080');
-// })
+app.post("/join-us", async (req, res) => {
+  let newAnha = new Anha(req.body);
+  try {
+    await newAnha.save();
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: err.message, status: "failed to save note" });
+  }
+  res.status(201).json(newAnha);
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("server started on " + port + "...");
+});
